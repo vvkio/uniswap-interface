afad18573ddf85124f18fb32412e8cb3eba21db4
Niko94 <nikoberetic@gmail.com>
2021-03-20 17:42:19 +0100

    style

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -59,19 +59,19 @@ export function colors(darkMode: boolean): Colors {
     advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
 
     //primary colors
-    primary1: darkMode ? '#2172E5' : '#ff007a',
-    primary2: darkMode ? '#3680E7' : '#FF8CC3',
-    primary3: darkMode ? '#4D8FEA' : '#FF99C9',
-    primary4: darkMode ? '#376bad70' : '#F6DDE8',
-    primary5: darkMode ? '#153d6f70' : '#FDEAF1',
+    primary1: darkMode ? '#2172E5' : '#0033ad',
+    primary2: darkMode ? '#3680E7' : '#3d5db8',
+    primary3: darkMode ? '#4D8FEA' : '#6d85c9',
+    primary4: darkMode ? '#376bad70' : '#9dadda',
+    primary5: darkMode ? '#153d6f70' : '#ced6ed',
 
     // color text
-    primaryText1: darkMode ? '#6da8ff' : '#ff007a',
+    primaryText1: darkMode ? '#6da8ff' : '#0033ad',
 
     // secondary colors
-    secondary1: darkMode ? '#2172E5' : '#ff007a',
-    secondary2: darkMode ? '#17000b26' : '#F6DDE8',
-    secondary3: darkMode ? '#17000b26' : '#FDEAF1',
+    secondary1: darkMode ? '#2172E5' : '#0033ad',
+    secondary2: darkMode ? '#17000b26' : '#9dadda',
+    secondary3: darkMode ? '#17000b26' : '#ced6ed',
 
     // other
     red1: '#FD4040',
@@ -177,15 +177,19 @@ export const TYPE = {
 }
 
 export const FixedGlobalStyle = createGlobalStyle`
+
+
+@import url('https://fonts.googleapis.com/css2?family=Chivo:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+
 html, input, textarea, button {
-  font-family: 'Inter', sans-serif;
-  font-display: fallback;
+  font-family: 'Chivo', sans-serif;
+  //font-display: fallback;
 }
-@supports (font-variation-settings: normal) {
-  html, input, textarea, button {
-    font-family: 'Inter var', sans-serif;
-  }
-}
+//@supports (font-variation-settings: normal) {
+  //html, input, textarea, button {
+  //  font-family: 'Inter var', sans-serif;
+  //}
+//}
 
 html,
 body {
